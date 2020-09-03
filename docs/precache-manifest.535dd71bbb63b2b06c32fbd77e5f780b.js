1b91bfba4548589e78448c5a12b05e89c4f604ec
Rafael Reyes <j24ym26@gmail.com>
2020-09-03 12:09:05 -0500

    Se sube las fuentes para produccion

---
+++ docs/precache-manifest.535dd71bbb63b2b06c32fbd77e5f780b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9b81809c780764582471a4f7336611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "459db031b711832c7e3e",
+    "url": "/static/css/main.4c0418bb.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459db031b711832c7e3e",
+    "url": "/static/js/main.a93d2ce3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
