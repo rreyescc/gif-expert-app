f11aa149c94f6033bacbda372266382cf2d4b3b3
Rafael Reyes <j24ym26@gmail.com>
2020-09-03 12:24:38 -0500

    Se actualizo el protocolo http por https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@ export const getGifs = async ( categoria ) => {
 
     //const obtenerImagenesGif = async () => {
         const apiKey = 'msRtlKG1L0ckRTwzSlkaNYZR1fMl3aDP';
-        const endPoint = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(categoria) }&limit=5&api_key=${apiKey}`;
+        const endPoint = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(categoria) }&limit=5&api_key=${apiKey}`;
 
         const response = await fetch(endPoint);
         const { data } = await response.json();
