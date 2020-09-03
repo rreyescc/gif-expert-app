25b3a10d4ab6a4b67984dba91d3eed706a037683
Rafael Reyes <j24ym26@gmail.com>
2020-09-03 12:44:20 -0500

    Se volvio a subir la carpeta docs

---
+++ docs/precache-manifest.99b60f88e900352f5217d91c04983822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1542a9688e3c66be6cd67d66d6882b52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "116bc56fef5ed47f82b5",
+    "url": "/static/css/main.4c0418bb.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116bc56fef5ed47f82b5",
+    "url": "/static/js/main.9cd76934.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
